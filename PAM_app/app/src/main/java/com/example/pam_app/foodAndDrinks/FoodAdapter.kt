b3c96e9964d30629ca2faec0b9adac6301e6eeb2
catea016 <catea.gherbovei@gmail.com>
2021-03-05 20:58:30 +0200

    adding items in the list

--- PAM_app/app/src/main/java/com/example/pam_app/foodAndDrinks/FoodAdapter.kt
+++ PAM_app/app/src/main/java/com/example/pam_app/foodAndDrinks/FoodAdapter.kt
@@ -2,30 +2,36 @@ package com.example.pam_app.foodAndDrinks
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pam_app.R
 
-internal class FoodAdapter(private var foodList: List<FoodModel>) :
-    RecyclerView.Adapter<FoodAdapter.MyViewHolder>() {
+internal class FoodAdapter(private var foodList: List<FoodModel>) : RecyclerView.Adapter<FoodAdapter.MyViewHolder>() {
+
     internal inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var image: TextView = view.findViewById(R.id.imageFood)
+        var image: ImageView = view.findViewById(R.id.imageFood)
         var name: TextView = view.findViewById(R.id.textFoodName)
         var price: TextView = view.findViewById(R.id.textPrice)
     }
+
     @NonNull
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item_grid_food, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item_grid_food, parent, false)
+
         return MyViewHolder(itemView)
     }
+
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val food = foodList[position]
-        holder.image.text = food.getImage()
+        food.getImage()?.let {
+            holder.image.setImageResource(it)
+        }
         holder.name.text = food.getName()
         holder.price.text = food.getPrice()
     }
+
     override fun getItemCount(): Int {
         return foodList.size
     }
--- PAM_app/app/src/main/java/com/example/pam_app/foodAndDrinks/FoodModel.kt
+++ PAM_app/app/src/main/java/com/example/pam_app/foodAndDrinks/FoodModel.kt
@@ -1,32 +1,53 @@
 package com.example.pam_app.foodAndDrinks
 
-class FoodModel(image: String?, name: String?, price: String?) {
-    private var image: String
+class FoodModel(image: Int?, name: String?, price: String?) {
+    private var image: Int?
     private var name: String
     private var price: String
+
     init {
-        this.image = image!!
+        this.image = image
         this.name = name!!
         this.price = price!!
     }
-    fun getImage(): String?{
+
+
+    fun getImage(): Int? {
         return image
     }
-    fun setImage(name: String?){
+
+    fun setImage(name: Int?) {
         image = name!!
     }
 
-    fun getName(): String?{
+    fun getName(): String? {
         return name
     }
-    fun setName(text: String?){
+
+    fun setName(text: String?) {
         name = text!!
     }
 
-    fun getPrice(): String?{
+    fun getPrice(): String? {
         return price
     }
-    fun setPrice(nr: String?){
+
+    fun setPrice(nr: String?) {
         price = nr!!
     }
+
+    /*
+    TODO: replace self written setter and getters with kotlin properties access https://kotlinlang.org/docs/properties.html
+    var setGetImage: Int? = image
+        get() = field
+        set(value) {
+            field = value
+        }
+    var setGetName: String? = name
+        get() = field.toString()
+        set(value) {
+            field = value.toString()
+        }
+*/
+
 }
